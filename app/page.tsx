4bb010a829123d2d1873d6d54b0ed4247cbbda99
Gabriel-Malenowitch <gabrielbotelhomalenowitch@gmail.com>
2024-06-09 12:56:56 -0300

    refactor: deprecate 3d feature

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Text, HStack, Heading, VStack, GridItem, Grid, Link, Button, UnorderedL
 
 import HeadphonesIcon from '@mui/icons-material/Headphones';
 import { ThreeJsModel } from "./threejs-model";
+import { DownloadCurriculumButton } from "./download-curriculum-buttom";
 
 export default function Home() {
   
@@ -11,20 +12,20 @@ export default function Home() {
           <HStack  justifyContent="space-between" style={{padding: "32px 16px"}}>
             <Heading as="h6" alignItems="center" display="flex">
               <HStack gap={4}>
-                {/* <ViewInArIcon fontSize="large" />  */}
                 <Text>
                   Portifólio
                 </Text>
                 <HeadphonesIcon fontSize="large" /> 
               </HStack>
             </Heading>
+              <DownloadCurriculumButton />
           </HStack>
         </header>
         <main>
           <section id="home">
             <VStack alignItems="start" fontSize="1.5rem">
               <Text>
-                {'"'}Nas linhas de código, descubro um portal para a imaginação, onde problemas se transformam em desafios emocionantes e cada bug é uma oportunidade para aprender. Meu código é minha voz, minha maneira de comunicar ideias e moldar a realidade. Como um artista com seu pincel, mergulho nas profundezas do meu IDE, criando obras-primas digitais que transcendem o tempo e o espaço.
+                {'"'}Nas linhas de código, descubro um portal para a imaginação, onde problemas se transformam em desafios emocionantes e cada bug é uma oportunidade para aprender. Meu código é minha voz, minha maneira de comunicar ideias e moldar a realidade. Como um artista com seu pincel, mergulho nas profundezas da minha lógica, criando obras-primas digitais que transcendem o tempo e o espaço.
               </Text>
               <br />
               <Text>
@@ -62,6 +63,7 @@ export default function Home() {
                 </ListItem>
                 </UnorderedList>
               </GridItem>
+              {/* // TODO: Adicionar modelo 3 aqui */}
               <GridItem>
                 {/* <ThreeJsModel /> */}
               </GridItem>
--- app/threejs-model.tsx
+++ app/threejs-model.tsx
@@ -1,18 +1,32 @@
-import { Canvas, useLoader } from "@react-three/fiber";
-import { OBJLoader } from "three/examples/jsm/Addons.js";
+'use client'
+import { useEffect, useLayoutEffect, useRef } from 'react'
+import * as t from 'three'
 
 
 export function ThreeJsModel() {
-    const obj = useLoader(OBJLoader, '../public/pc-obj/pc.obj');
+    const scene = new t.Scene()
+    const geometry = new t.SphereGeometry(3, 64, 64)
+    const material = new t.MeshStandardMaterial({ color: '#00ff83' })
+    const mesh = new t.Mesh(geometry, material)
+    scene.add(mesh)
 
-    return (
-        <Canvas
-            camera={{
-                position: [-6, 7, 7],
-            }}
-        >
-        <ambientLight color={"white"} intensity={0.3} />
-        <primitive object={obj} />
-        </Canvas>
-    )
+    const light = new t.PointLight("#ffffff", 1, 100)
+    light.position.set(0, 10, 10)
+    scene.add(light)
+
+    const camera = new t.PerspectiveCamera(45, 300, 300)
+    camera.position.z = 20
+    scene.add(camera)
+
+    const canvas = document.createElement('canvas')
+    const renderer = new t.WebGLRenderer({ canvas })
+    renderer.setSize(300, 300)
+    renderer.render(scene, camera)
+
+    useLayoutEffect(() => {
+        document.querySelector('#ThreeJsModelContainer')?.appendChild(canvas)
+    }, [canvas])
+
+    return <div id="ThreeJsModelContainer"></div>
+    
 }
